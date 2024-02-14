6697d74149fe69123304cb95d18bbc422d4d9953
mohamine78 <148878982+mohamine78@users.noreply.github.com>
2024-02-14 17:04:00 +0100

    Add files via upload
    Modèle Vue Template

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,31 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from listings.models import Band
+from listings.models import Listing
+
 
 # Create your views here.
+
+def hello(request):
+    bands = Band.objects.all()
+    return render(request, 'listings\hello.html',
+                  context={"bands":bands})
+
+def about(request):
+    return HttpResponse('<h1>About us</h1><p>Nous adorons Django</p>')
+
+def contact(request):
+    return HttpResponse('<h1>Contactez Nous!<h1><p>Pour plus de détails, veuillez nous contacter!</p>')
+
+def list(request):
+    titles = Listing.objects.all()
+    return HttpResponse(f"""
+                        <h1>Hello Django !</h1>
+                        <p>Mes groupes préférés sont :</p>
+                        <ul>
+                        <li>{titles[0].title}</li>
+                         <li>{titles[1].title}</li>
+                          <li>{titles[2].title}</li>
+                          <li>{titles[3].title}</li>
+                        </ul>
+                        """)
